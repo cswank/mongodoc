3badc87afeee5ef7d2c891b7d0e5609ac8bcc440
Craig <craig.swank@nrel.gov>
2011-08-02 10:05:53 -0600

    cleaning up the code

--- mongodoc/mongodoc.py
+++ mongodoc/mongodoc.py
@@ -1,19 +1,27 @@
-
-
 class MongoDoc(object):
+    """
+    Takes a dictionary and documents the type for each key.
+    If the value for a key is a dict (or a list of dicts)
+    then the sub-dict is documented as well.
+    """
 
     _ROW_BUFFER = 10
 
-    def __init__(self, doc, name, x=0, y=0, inlist=False):
+    def __init__(self, doc, name, inlist=False):
+        """
+        
+        Arguments:
+        - `doc`: the dictionary that is to be documented
+        - `name`: the name of the collection
+        - `inlist`: did this dict come from a list?
+        """
+
         self._doc = doc
-        self._x = x
-        self._y = y
         if inlist:
             self._name = '{0} <list>'.format(name)
         else:
             self._name = name
         self._max_row_width = 0
-        self._height = 0
         self._subdocs = []
         self._subdoc_rows = None
         self._rows = []
@@ -21,20 +29,14 @@ class MongoDoc(object):
         self.get_subdocs(doc)
         self._width = self._get_width()
 
-    def _get_width(self):
-        subdoc_width = max([d._width for d in self._subdocs]) if len(self._subdocs) > 0 else 0
-        return self._max_row_width + subdoc_width
-
     @property
     def text(self):
         return '\n'.join([row for row in self.rows])
 
     @property
     def rows(self):
-        yield self.header[0].format('', width=self._width)
-        yield self.header[1].format('', width=self._width - len(self._name) - 1)
-        yield self.header[2].format('', width=self._width)
-        sub_rows = None
+        for item in self._get_header():
+            yield item
         for row in self._rows:
             subdoc_row = self._get_subdoc_row()
             if subdoc_row is not None:
@@ -46,16 +48,9 @@ class MongoDoc(object):
         while subdoc_row is not None:
             yield self._get_row_with_subdoc(['',''], subdoc_row)
             subdoc_row = self._get_subdoc_row()
-        yield self.footer.format('', width=self._width)
+        yield self._get_footer()
 
     def _get_row_with_subdoc(self, row, subdoc_row):
-        """
-        | key: value      | subdoc key: subdoc value  ||
-                          |                            |
-                          |                            |
-                          w1 = self._width - len key/value  |
-                                                       w2 = self.width - len(subdoc_row)
-        """
         if row[0] == '':
             kv = ''
         else:
@@ -72,25 +67,21 @@ class MongoDoc(object):
         width = self._max_row_width - len(row[0]) - self._ROW_BUFFER
         kv = '{0}: {1: >{width}}'.format(row[0], row[1], width=width)
         width = self._width - len(kv) - 1
-        if width < 0:
-            width = 0
         return '| {0}{1: <{width}}|'.format(
             kv,
             ' ',
             width=width,
             )
 
-    @property
-    def header(self):
+    def _get_header(self):
         return [
-            ' {0:_>{width}}',
-            '| {0}'.format(self._name) + '{0: >{width}}|',
-            '|{0:_>{width}}|',
+            ' {0:_>{width}}'.format('', width=self._width),
+            '| {0}'.format(self._name) + '{0: >{width}}|'.format('', width=self._width - len(self._name) - 1),
+            '|{0:_>{width}}|'.format('', width=self._width),
             ]
 
-    @property
-    def footer(self):
-        return '|{0:_>{width}}|'
+    def _get_footer(self):
+        return '|{0:_>{width}}|'.format('', width=self._width)
 
     def get_subdocs(self, doc):
         for key, value in doc.iteritems():
@@ -99,14 +90,12 @@ class MongoDoc(object):
             elif isinstance(value, list) and len(value) > 0 and isinstance(value[0], dict):
                 self._subdocs.append(MongoDoc(value[0], key, inlist=True))
             
-
     def get_rows(self, doc):
         for key, value in doc.iteritems():
             row = (key, str(type(value)))
             l = len(row[0]) + len(row[1]) + self._ROW_BUFFER
             if l > self._max_row_width:
                 self._max_row_width = l
-            self._height += 1
             self._rows.append(row)
 
     def _get_subdoc_row(self):
@@ -121,3 +110,7 @@ class MongoDoc(object):
             self._subdoc_rows = None
             return
         return row
+
+    def _get_width(self):
+        subdoc_width = max([d._width for d in self._subdocs]) if len(self._subdocs) > 0 else 0
+        return self._max_row_width + subdoc_width
---
+++ mongodoc/scripts.py
@@ -0,0 +1,67 @@
+import argparse
+import os
+from .mongodoc import MongoDoc
+from pymongo import Connection
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Document a mongo db')
+    parser.add_argument('--port', default=27017, type=int, help='The db port number')
+    parser.add_argument('--host', type=str, default='localhost', help='The db host')
+    parser.add_argument('--username', type=str, help='The username for authenticating to the db')
+    parser.add_argument('--password', type=str, help='The password for authenticating to the db')
+    parser.add_argument('--name', required=True, type=str, help='The name of the mongo db to document')
+    parser.add_argument('--file', type=str, help='The name of the output file')
+    return parser.parse_args()
+
+def get_db(host, port, name, username=None, password=None):
+    connection = Connection(host=host, port=port)
+    db = connection[name]
+    if username is not None and password is not None:
+        db.authenticate(username, password)
+    return db
+
+def document_collection(db, name):
+    cursor = db[name].find()
+    if cursor.count() == 0:
+        return ''
+    while True:
+        try:
+            doc = cursor.next()
+        except StopIteration:
+            print 'sorry, that was the last doc in this collection and it will be used for the output.'
+            break
+        mongo_doc = MongoDoc(doc, name)
+        text = mongo_doc.text
+        print text
+        answer = raw_input('Use this doc (y/n)?')
+        if answer != 'n':
+            break
+    return text
+
+def write_output(text, args):
+    filename = args.file
+    if filename is None:
+        filename = '{0}-mongodoc.txt'.format(args.name)
+    if os.path.exists(filename):
+        overwrite = raw_input('{0} already exists.  overwrite (y/n)? '.format(filename))
+        if overwrite != 'y':
+            print 'aborting'
+            return
+    f = open(filename, 'w')
+    f.write(text)
+    f.close()
+    print 'The results were written to {0}'.format(filename)
+
+def document_db():
+    args = get_args()
+    db = get_db(args.host, args.port, args.name)
+    text = ''
+    for name in db.collection_names():
+        if name.startswith('system.'):
+            continue
+        text += document_collection(db, name) + '\n\n'
+    write_output(text, args)
+    
+        
+    
+
