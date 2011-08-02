80ba11f707daac7e71ac9b232318f8d3f81e4aa5
Craig <craig.swank@nrel.gov>
2011-08-02 07:24:32 -0600

    getting there

--- mongodoc/mongodoc.py
+++ mongodoc/mongodoc.py
@@ -7,68 +7,102 @@ class MongoDoc(object):
         self._x = x
         self._y = y
         self._name = name
-        self._width = 0
+        self._max_row_width = 0
         self._height = 0
         self._subdocs = []
-        self._lines = []
-        self.get_lines(doc)
+        self._subdoc_rows = None
+        self._rows = []
+        self.get_rows(doc)
         self.get_subdocs(doc)
+        self._width = self._get_width()
+
+
+    def _get_width(self):
+        subdoc_width = max([d._width for d in self._subdocs]) if len(self._subdocs) > 0 else 0
+        return self._max_row_width + subdoc_width
 
     @property
     def text(self):
-        return '\n'.join([line for line in self.lines])
+        return '\n'.join([row for row in self.rows])
 
     @property
-    def lines(self):
-        for line in self.header:
-            yield line.format('', width=self.width)
-        sub_lines = None
-        for line in self._lines:
-            if len(self._subdocs) > 0:
-                sub_lines = self._subdocs.pop().lines
-            if sub_lines is not None:
-                try:
-                    sub_line = sub_lines.next()
-                except StopIteration:
-                    
-                line = '| {0}: {1: >{width}} {2} |'.format(line[0], line[1], self._subdocs[0].lines.next(), width=self._width)
+    def rows(self):
+        yield self.header[0].format('', width=self._width)
+        yield self.header[1].format('', width=self._width - len(self._name) - 1)
+        yield self.header[2].format('', width=self._width)
+        sub_rows = None
+        for row in self._rows:
+            subdoc_row = self._get_subdoc_row()
+            if subdoc_row is not None:
+                row = self._get_row_with_subdoc(row, subdoc_row)
             else:
-                line = '| {0}: {1: >{width}} |'.format(line[0], line[1], width=self.width)
-            yield line
-        yield self.footer.format('', width=self.width)
+                row = self._get_row_without_subdoc(row)
+            yield row
+        yield self.footer.format('', width=self._width)
 
-    @property
-    def width(self):
-        if len(self._subdocs) > 0:
-            return self._width + max([d.width for d in self._subdocs])
-        return self._width
+    def _get_row_with_subdoc(self, row, subdoc_row):
+        """
+        | key: value      | subdoc key: subdoc value  ||
+                          |                            |
+                          |                            |
+                          w1 = self._width - len key/value  |
+                                                       w2 = self.width - len(subdoc_row)
+        """
+        kv = '{0}: {1}'.format(row[0], row[1])
+        width = self._width - len(kv) - 2
+        return '| {0} {1: >{width}}|'.format(
+            kv,
+            subdoc_row,
+            width=width
+            )
+
+    def _get_row_without_subdoc(self, row):
+        kv = '{0}: {1}'.format(row[0], row[1])
+        return '| {0}{1: <{width}}|'.format(
+            kv,
+            ' ',
+            width=self._width - len(kv) - 1
+            )
 
     @property
     def header(self):
         return [
-            '{0:_>{width}}',
+            ' {0:_>{width}}',
             '| {0}'.format(self._name) + '{0: >{width}}|',
             '|{0:_>{width}}|',
             ]
 
     @property
     def footer(self):
-        return '{0:_>{width}}'
+        return '|{0:_>{width}}|'
 
     def get_subdocs(self, doc):
         for key, value in doc.iteritems():
             if isinstance(value, dict):
                 self._subdocs.append(MongoDoc(value, key))
 
-    def get_lines(self, doc):
+    def get_rows(self, doc):
         for key, value in doc.iteritems():
-            #line = '| {0}: {1: >{width}}'.format(key, str(type(value)), width=60 - len(key))
-            line = (key, str(type(value)))
-            l = len(line[0]) + len(line[1]) + 5
-            if l > self._width:
-                self._width = l
+            #row = '| {0}: {1: >{width}}'.format(key, str(type(value)), width=60 - len(key))
+            row = (key, str(type(value)))
+            l = len(row[0]) + len(row[1]) + 5
+            if l > self._max_row_width:
+                self._max_row_width = l
             self._height += 1
-            self._lines.append(line)
+            self._rows.append(row)
+
+    def _get_subdoc_row(self):
+        if self._subdoc_rows is None and len(self._subdocs) == 0:
+            return
+        if self._subdoc_rows is None:
+            subdoc = self._subdocs.pop(0)
+            self._subdoc_rows = subdoc.rows
+        try:
+            row = self._subdoc_rows.next()
+        except StopIteration:
+            return
+        return row
+            
                 
 
     
