e049beddb6571d2eb8483f587d9ba43eb5ffab65
Craig <craig.swank@nrel.gov>
2011-12-02 10:43:06 -0700

    incrementing version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.3.1'
+version = '0.3.2'
 
 def read():
     readme = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
